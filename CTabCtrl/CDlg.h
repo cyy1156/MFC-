e9d13e883afb7698193e414b86df937540cf0e88
Your Name <you@example.com>
2026-02-12 11:57:04 +0800

    添加所有新增文件夹和文件

---
+++ CTabCtrl/CDlg.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CDlg 对话框
+
+class CDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlg)
+
+public:
+	CDlg(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG2 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
